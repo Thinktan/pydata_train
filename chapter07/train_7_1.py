5b88991cd31ae471dfe61c6cd15db87bd34842bd
Thinktan <tantao0675@hnu.edu.cn>
2023-05-14 16:47:29 +0800

    变更

--- chapter07/train_7_1.py
+++ chapter07/train_7_1.py
@@ -1,6 +1,14 @@
 
 import pandas as pd
-import numpy as np
+from numpy import nan as NA
 
-string_data = pd.Series(['aardvark', 'artichoke', np.nan, 'avocado'])
+# string_data = pd.Series(['aardvark', 'artichoke', np.nan, 'avocado'])
 
+data = pd.DataFrame([[1., 6.5, 3.], [1, NA, NA], [NA, NA, NA], [NA, 6.5, 3.]])
+print(data, '\n')
+
+cleaned = data.dropna()
+print(cleaned, '\n')
+
+cleaned = data.dropna(how='all')
+print(cleaned, '\n')
