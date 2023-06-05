1b06f207911dd0a20f0e925105832bc413aeb0ae
Thinktan <55999974+Thinktan@users.noreply.github.com>
2023-06-05 13:01:41 +0800

    up from pro

--- chapter11/train_11_5_4.py
+++ chapter11/train_11_5_4.py
@@ -1,1 +1,14 @@
 import pandas as pd
+import numpy as np
+
+# 从数组生成PeriodIndex
+
+data = pd.read_csv('../examples/macrodata.csv')
+print(data)
+print(data.year)
+print(data.quarter)
+
+index = pd.PeriodIndex(year=data.year, quarter=data.quarter, freq='Q-DEC')
+print(index)
+data.index = index
+print(data.infl)
